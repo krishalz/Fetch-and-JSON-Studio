1f2e10660edd4036586b68ae59c0aa02da8397a5
kayhals <kayhals4@gmail.com>
2023-07-04 18:55:00 -0500

    completed Fetch-and-JSON-except-not-bonuses

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-// TODO: add code here
+window.addEventListener("load", function() {
+    fetch("https://handlers.education.launchcode.org/static/astronauts.json").then(function(response) {
+    response.json().then( function(json) {
+    
+    for (index=0; index < json.length; index++) {
+        const div = document.getElementById("container");
+       
+        div.innerHTML = div.innerHTML += `
+        <div class="astronaut">
+          <div class="bio">
+         <h3>${json[index].firstName} ${json[index].lastName}</h3>
+           <ul>
+            <li>Hours in space: ${json[index].hoursInSpace} </li>
+            <li>Active: ${json[index].active}</li>
+            <li>Skills: ${json[index].skills} </li>
+           
+           </ul>
+         </div>
+            <img class="avatar" src= ${json[index].picture}>
+        </div>   
+        `
+    }
+    });
+ 
+    });
+   
+  }); 
+
+ 
+
